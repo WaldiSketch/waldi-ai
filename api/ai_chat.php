b8d5b7c62820d78151a51f758b8c7548783ba37b
WaldiSketch <halobrojangannangis@gmail.com>
2026-02-02 15:08:43 +0700

    Revert "gemini to gemma"
    This reverts commit 8b1db6b6de7b0750aaaeb64e1c2fe4d6b2705fe0.

--- api/ai_chat.php
+++ api/ai_chat.php
@@ -7,7 +7,7 @@ header("Content-Type: application/json");
 // =====================
 // DAPATKAN API KEY DI: https://aistudio.google.com/
 $API_KEY = getenv('GEMINI_API_KEY') ?: 'AIzaSyDu_4P5d4ubBCshGHasGzPK8m9GHTUyo_Q'; 
-$model = "gemma-3-27b-it";
+$model = "gemini-2.5-flash";
 
 // =====================
 // AMBIL INPUT
